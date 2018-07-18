8ff069e134eee1af3c1ce2cc8d5b3f2060614bc9
hannah <1065316432@qq.com>
2018-07-18 11:24:04 +0800

    临时生成bundle到public

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
     entry: './src/routes.tsx',
   
     output: {
-        path: path.resolve(__dirname, 'dist'),        
+        path: path.resolve(__dirname, 'public'),        
         filename: 'bundle.js',
         publicPath: '/'
     },
